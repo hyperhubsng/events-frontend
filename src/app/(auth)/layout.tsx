54a9c96ecdd7243ab181ae0822fdb0f453da966d
Kelvin Ochubili <kelvinochubili@gmail.com>
2025-03-10 10:21:47 +0100

    feat: add auth layout for login and signup screens

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const AuthLayout = () => {
-	return <div>AuthLayout</div>;
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<main className='bg-[url(/images/auth-bg.png)] bg-cover bg-no-repeat'>
+			{children}
+		</main>
+	);
 };
 
 export default AuthLayout;
