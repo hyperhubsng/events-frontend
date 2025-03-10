f1f732b85a258198a579019b18f1e4839436a04e
Kelvin Ochubili <kelvinochubili@gmail.com>
2025-03-10 11:45:52 +0100

    feat: add logo to auth layout

--- src/_pages/auth/login.tsx
+++ src/_pages/auth/login.tsx
@@ -1,5 +1,13 @@
 const Login = () => {
-	return <div>login</div>;
+	return (
+		<div>
+			<div className='border-b border-solid border-b-[#E8E8E8] py-3 mt-4'>
+				<h1 className='text-[#101010] text-2xl md:text-[1.75rem] text-center font-semibold'>
+					Welcome Admin
+				</h1>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
@@ -5,6 +6,15 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
 		<main className='bg-[url(/images/auth-bg.png)] bg-cover bg-no-repeat'>
 			<div className='flex items-center justify-center h-dvh px-6'>
 				<div className='bg-white max-w-[38.5rem] w-full rounded-[8px] p-10'>
+					<div className='flex items-center justify-center'>
+						<Image
+							src='/icons/logo.svg'
+							width={186}
+							height={32}
+							alt='hyperhubs logo'
+							className='w-[140px] md:w-auto'
+						/>
+					</div>
 					{children}
 				</div>
 			</div>
