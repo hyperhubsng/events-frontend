b9b65f63c826bafaae42eb24550e3af9b5ba8194
Kelvin Ochubili <kelvinochubili@gmail.com>
2025-03-11 10:08:30 +0100

    fix: active link icon fix for events page

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -86,7 +86,10 @@ export function AppSidebar() {
 															viewBox='0 0 24 24'
 															fill='none'
 															xmlns='http://www.w3.org/2000/svg'
-															className='fill-current group-hover/trigger:fill-none'>
+															className={cn(
+																' group-hover/trigger:fill-none',
+																link.href !== pathname && 'fill-current'
+															)}>
 															<path
 																d='M20.25 4.5H3.75C3.33579 4.5 3 4.83579 3 5.25V18.75C3 19.1642 3.33579 19.5 3.75 19.5H20.25C20.6642 19.5 21 19.1642 21 18.75V5.25C21 4.83579 20.6642 4.5 20.25 4.5Z'
 																stroke='#003366'
