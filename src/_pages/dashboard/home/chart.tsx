237377e384744287324e9d4e57c451c5b4d19420
Kelvin Ochubili <kelvinochubili@gmail.com>
2025-03-13 11:04:40 +0100

    feat: implement line chart with custom tooltip and add chart data

--- src/_pages/dashboard/home/chart.tsx
+++ src/_pages/dashboard/home/chart.tsx
@@ -8,13 +8,27 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from '@/components/ui/select';
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts';
+import { ChartConfig, ChartContainer, ChartTooltip } from '@/components/ui/chart';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Calendar } from '@/components/ui/calendar';
 import { DateRange } from 'react-day-picker';
 import { format } from 'date-fns';
 import { ChevronDown } from 'lucide-react';
 
+import * as RechartsPrimitive from 'recharts';
+
 import Tabs from '@/components/tabs';
+
+const chartConfig = {
+	total_revenue: {
+		label: 'Total Revenue',
+		color: 'var(--blue-50)',
+	},
+	sales: {
+		label: 'Sales',
+	},
+} satisfies ChartConfig;
 
 const Chart = () => {
 	const [selected, setSelected] = useState('Revenue');
@@ -25,6 +39,17 @@ const Chart = () => {
 
 	const [time, setTime] = useState('This Week');
 	const times = ['This Week', 'Last Week', 'Two weeks ago'];
+
+	const chartData = [
+		{ day: 'Mar 01, 2025 04:00PM', total_revenue: 5, sales: 150000 },
+		{ day: 'Mar 02, 2025 04:00PM', total_revenue: 5, sales: 200000 },
+		{ day: 'Mar 03, 2025 04:00PM', total_revenue: 30, sales: 80000 },
+		{ day: 'Mar 04, 2025 04:00PM', total_revenue: 40, sales: 900000 },
+		{ day: 'Mar 05, 2025 04:00PM', total_revenue: 0, sales: 250000 },
+		{ day: 'Mar 06, 2025 04:00PM', total_revenue: 48, sales: 320000 },
+		{ day: 'Mar 07, 2025 04:00PM', total_revenue: 55, sales: 200000 },
+		{ day: 'Mar 08, 2025 04:00PM', total_revenue: 0, sales: 650000 },
+	];
 
 	return (
 		<div className='bg-white rounded-[4px] md:rounded-[8px] pt-3 md:pt-5 px-3 md:px-6 pb-6 mt-4'>
@@ -40,7 +65,7 @@ const Chart = () => {
 						<PopoverTrigger asChild>
 							<button
 								className='flex items-center justify-between md:gap-4 border text-sm text-black-950
-              border-white-300 border-solid rounded-[4px] px-3 h-8 w-full md:w-fit'>
+              border-white-300 border-solid rounded-[4px] px-3 h-8 '>
 								{date?.from ? (
 									date.to ? (
 										<>
@@ -72,7 +97,7 @@ const Chart = () => {
 					<Select defaultValue={time} onValueChange={(e) => setTime(e)}>
 						<SelectTrigger
 							className='focus-visible:ring-0 focus-visible:outline-none text-sm text-black-950
-             shadow-none h-8 border-white-300 focus-visible:border-white-300 select-chevron w-full md:w-fit'>
+             shadow-none h-8 border-white-300 focus-visible:border-white-300 select-chevron '>
 							<SelectValue />
 						</SelectTrigger>
 						<SelectContent className='w-full'>
@@ -88,8 +113,61 @@ const Chart = () => {
 					</Select>
 				</div>
 			</div>
+
+			<div className='mt-8 -ml-10'>
+				<ChartContainer config={chartConfig} className='max-h-[315px] w-full'>
+					<LineChart
+						accessibilityLayer
+						data={chartData}
+						margin={{
+							left: 12,
+							right: 12,
+						}}>
+						<CartesianGrid vertical={false} horizontal={true} />
+						<XAxis
+							dataKey='day'
+							axisLine={false}
+							tickMargin={8}
+							tickFormatter={(value) => value.slice(0, 6)}
+							interval={1}
+						/>
+						<YAxis />
+						<ChartTooltip cursor={false} content={<CustomTooltip />} />
+						<Line
+							dataKey='total_revenue'
+							type='linear'
+							stroke='var(--color-total_revenue)'
+							strokeWidth={2}
+							dot={{
+								stroke: 'var(--color-total_revenue)',
+							}}
+							// activeDot={{
+							// 	r: 6,
+							// }}
+						/>
+					</LineChart>
+				</ChartContainer>
+			</div>
 		</div>
 	);
 };
 
 export default Chart;
+
+const CustomTooltip = ({
+	active,
+	payload,
+}: React.ComponentProps<typeof RechartsPrimitive.Tooltip>) => {
+	if (active && payload && payload.length) {
+		const item = payload[0].payload as { sales: number; day: string };
+
+		return (
+			<div className='bg-white rounded-[4px] border border-solid border-white-200 text-center py-1 px-2'>
+				<p className='text-black-950 text-xs'>{item.day}</p>
+				<p className='text-black-950 text-sm font-bold pt-2'>
+					N{item.sales.toLocaleString()}
+				</p>
+			</div>
+		);
+	}
+};
--- src/components/info-card.tsx
+++ src/components/info-card.tsx
@@ -9,10 +9,12 @@ const InfoCard: React.FC<CardProps> = ({ icon, title, info }) => {
 		<div
 			className='bg-white h-[53px] rounded-[4px] 
     md:h-[93px] md:rounded-[8px] flex items-center px-3 md:px-6'>
-			<div className='flex items-center gap-4'>
+			<div className='flex items-center gap-2 md:gap-4'>
 				{icon}
 				<div className='flex flex-col'>
-					<h2 className='text-xs md:text-sm text-black-950 font-medium'>{title}</h2>
+					<h2 className='text-xs md:text-sm text-black-950 font-medium whitespace-nowrap'>
+						{title}
+					</h2>
 					<h3 className='text-base md:text-[1.25rem] text-black-950 font-bold'>
 						{info}
 					</h3>
